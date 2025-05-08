09a47ac900260c56c81545293be61dc1da64a0f1
ModelBandit <129765493+ModelBandit@users.noreply.github.com>
2025-05-08 12:05:08 +0900

    first commit

---
+++ FirstJava/src/chapter01/Addition.java
@@ -0,0 +1,7 @@
+package chapter01;
+
+public class Addition {
+	public static int Add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ FirstJava/src/chapter01/FirstClass.java
@@ -0,0 +1,18 @@
+package chapter01;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("우효~☆");
+
+		// sysout ctrl + space
+		// System.out.println();
+
+		System.out.println(Addition.Add(1, 2));
+
+		Addition addition = new Addition();
+		System.out.println(addition.Add(1, 2));
+	}
+
+}
