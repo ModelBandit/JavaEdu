5e7bf7dcd83da84ab9634f0d75dc15420d75d544
ModelBandit <129765493+ModelBandit@users.noreply.github.com>
2025-05-14 13:16:20 +0900

    버블정렬

--- FirstJava/src/myAlgorithm/MainClass.java
+++ FirstJava/src/myAlgorithm/MainClass.java
@@ -3,6 +3,8 @@ package myAlgorithm;
 public class MainClass {
 
 	public static void main(String[] args) {
+		int[] arr = {59,10,30,2,16,8,31,22};
 		
+		MySort.bubbleSort(arr);
 	}
 }
--- FirstJava/src/myAlgorithm/MySort.java
+++ FirstJava/src/myAlgorithm/MySort.java
@@ -3,6 +3,30 @@ package myAlgorithm;
 import java.util.Scanner;
 
 public class MySort {
+	
+	public static void bubbleSort(int[] arr) {
+		System.out.print("\nInput Data: ");
+		for(int i = 0;i<arr.length;++i) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		
+		for(int i = 0;i<arr.length;++i) {
+			for(int j = i; j < arr.length;++j) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+			System.out.print(i+1 + "차 정렬: ");
+			for(int j = 0 ; j<arr.length;++j) {
+				System.out.print(arr[j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
 	public static void selectionSort() {
 		Scanner scanner = new Scanner(System.in);
 
